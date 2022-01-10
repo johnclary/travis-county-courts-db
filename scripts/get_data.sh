14317c4d96fdfbd33d285468426140cda723a463
John Clary <john.clary@austintexas.gov>
2022-01-09 23:27:25 -0600

    this will help

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# 2 5 * * * sudo cd /home/ec2-user/travis-county-courts-db/scripts/ && ./get_data.sh
+# 2 5 * * * sudo bash /home/ec2-user/travis-county-courts-db/scripts/get_data.sh > /tmp/johncron
 docker run -d --rm --env-file /home/ec2-user/travis-county-courts-db/scripts/env_file -v /home/ec2-user/travis-county-courts-db/scripts:/app evictions python get_new_civil_cases.py
