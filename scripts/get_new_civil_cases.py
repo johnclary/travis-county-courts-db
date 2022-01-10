255587292da14367d2ee9c6fd486bfa53e03953a
John Clary <john.clary@austintexas.gov>
2022-01-09 22:44:37 -0600

    hrumph

--- scripts/get_new_civil_cases.py
+++ scripts/get_new_civil_cases.py
@@ -1,6 +1,7 @@
 import argparse
 from datetime import datetime, timedelta
 import logging
+from logging.handlers import RotatingFileHandler
 import time
 import sys
 
@@ -171,7 +172,7 @@ def get_logger():
     formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s: %(message)s")
     stream_handler = logging.StreamHandler(stream=sys.stdout)
     stream_handler.setFormatter(formatter)
-    file_handler = logging.handlers.RotatingFileHandler('log.log', maxBytes=20000, backupCount=5)
+    file_handler = RotatingFileHandler('log.log', maxBytes=20000, backupCount=5)
     file_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     logger.addHandler(file_handler)
